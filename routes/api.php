9b7cec3f0477a7f68af7a4a0add022992613d16a
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-25 05:07:21 +0200

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\API\AdminController;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\MembershipController;
+use App\Http\Controllers\API\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,9 +27,21 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('admin/users', [AdminController::class,'userIndex']);
     Route::put('admin/users/{id}', [AdminController::class,'makeAdmin']);
     Route::delete('admin/users/{id}', [AdminController::class,'deleteUser']);
+
+    Route::post('admin/trips/{id}', [TripController::class,'store']);
+    Route::delete('admin/trips/{id}', [TripController::class,'destroy']);
+    Route::put('admin/trips/{id}', [TripController::class,'update']);
 });
 
 
 
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
+
+
+Route::get('/memberships', [MembershipController::class,'index']);
+Route::get('/memberships/{id}', [MembershipController::class,'show']);
+
+
+Route::get('/trips', [TripController::class,'index']);
+Route::get('/trips/{id}', [TripController::class,'show']);
