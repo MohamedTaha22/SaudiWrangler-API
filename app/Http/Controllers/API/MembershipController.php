503de60afae5d7a18664375da2a9f248c52938e2
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-25 05:05:47 +0200

    add index and show

--- app/Http/Controllers/API/MembershipController.php
+++ app/Http/Controllers/API/MembershipController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Membership;
 use Illuminate\Http\Request;
 
 class MembershipController extends Controller
 {
-    //
+    public function index()
+    {
+        $memberships = Membership::all();
+
+        return  $memberships;
+    }
+
+    public function show($id)
+    {
+        $membership = Membership::find($id);
+        return $membership;
+    }
+
 }
